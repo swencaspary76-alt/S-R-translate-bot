a54f4d3a785649e31c9520f01ea97b5965c307f7
swencaspary76-alt <swencaspary76@gmail.com>
2026-03-24 10:41:10 +0100

    Refactor command registration in deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,32 +1,27 @@
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v10');
-const { SlashCommandBuilder } = require('discord.js');
+const { REST, Routes } = require('discord.js');
+const fs = require('fs');
 
-const commands = [
-  new SlashCommandBuilder().setName('translate').setDescription('Öffnet das Übersetzungs-Dashboard').toJSON()
-];
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '10' }).setToken('DEIN_BOT_TOKEN');
 
 (async () => {
   try {
-    console.log('Registriere Slash-Commands...');
-    if (!process.env.CLIENT_ID) throw new Error('CLIENT_ID ist nicht gesetzt.');
+    console.log('🔄 Registriere Slash Commands...');
 
-    if (process.env.GUILD_ID) {
-      await rest.put(
-        Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
-        { body: commands }
-      );
-      console.log('Guild commands registered.');
-    } else {
-      await rest.put(
-        Routes.applicationCommands(process.env.CLIENT_ID),
-        { body: commands }
-      );
-      console.log('Global commands registered.');
-    }
-  } catch (err) {
-    console.error('Fehler beim Registrieren der Commands:', err);
+    await rest.put(
+      Routes.applicationCommands('DEINE_CLIENT_ID'),
+      { body: commands }
+    );
+
+    console.log('✅ Slash Commands registriert!');
+  } catch (error) {
+    console.error(error);
   }
 })();
